ad3a342c0ced0224cffda9efe9371aea3899f34e
Maryam Nasseri <46525096+Maryam-Nasseri@users.noreply.github.com>
2024-08-22 22:30:37 +0100

    Create dependencies.py

---
+++ dependencies.py
@@ -0,0 +1,12 @@
+# below versions work better together with Python3.10
+%pip install --upgrade numpy==1.26.4
+%pip install --upgrade pandas==2.1.4
+%pip install --upgrade transformers==4.38.2
+%pip install --upgrade datasets==2.20.0
+%pip install --upgrade evaluate==0.4.2
+%pip install --upgrade scikit-learn==1.3.2
+%pip install --upgrade tensorflow==2.16.2
+%pip install --upgrade torch==2.2.1
+%pip install --upgrade huggingface_hub==0.23.4
+%pip install -U 'accelerate==0.27.2'
+%pip install --upgrade tf-keras==2.16.0
