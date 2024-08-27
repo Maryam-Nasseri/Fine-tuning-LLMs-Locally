f6756fff14c90f62505e533667f442c393f85819
Maryam Nasseri <46525096+Maryam-Nasseri@users.noreply.github.com>
2024-08-27 13:27:13 +0100

    Update training.py

--- training.py
+++ training.py
@@ -1,2 +1,27 @@
-# check this link for all training arguments: (training arguments)[https://huggingface.co/docs/transformers/en/main_classes/trainer#transformers.TrainingArguments.adam_beta2] 
+# check this link for all training arguments: https://huggingface.co/docs/transformers/en/main_classes/trainer#transformers.TrainingArguments.adam_beta2
+# check video tutorial for the implementation: 
+
+from transformers import Trainer, TrainingArguments
+
+training_args = TrainingArguments(
+    output_dir='ModelOutput', 
+    evaluation_strategy="epoch",
+    num_train_epochs=1,
+    learning_rate=2e-5, 
+    per_device_train_batch_size=8, 
+    per_device_eval_batch_size=8, 
+    weight_decay=0.01,
+    adam_beta1=0.9,
+    adam_beta2=0.999,
+    no_cuda=True 
+) 
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=training_dataset,
+    eval_dataset=evaluation_dataset,
+)
+
+trainer.train()
 
